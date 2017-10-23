0b53959292fdcc0d6ffd93e40c39942d8c329a00
neverclear86 <neverclear86sab@gmail.com>
2017-10-23 19:55:10 +0900

    とりあえず

---
+++ dig3.lua
@@ -0,0 +1,58 @@
+if require == nil then
+  function require(filename)
+    local h = fs.open(filename, "r")
+    local str = h.readAll()
+    h.close()
+    return assert(loadstring(str))()
+  end
+end
+
+log = require("/neverclear/apis/log")
+local move = require("/neverclear/apis/move")
+local dig, place = require("/neverclear/apis/block")
+local inventory = require("/neverclear/apis/inventory")
+local getopt = require("/neverclear/util/getopt")
+
+local torch = "minecraft:torch"
+
+log.file = "/logs/mine3.log"
+log.level = "info"
+
+local function digup()
+  dig.up(true)
+end
+
+local function digforward()
+  dig.forward(true)
+end
+
+local function digdown()
+  dig.down(true)
+end
+
+
+
+function main(args, opt)
+  local shouldUseTorch = opt.t or opt.torch
+  local n = tonumber(args[1])
+  local i = 0
+  while i < n do
+    parallel.waitForAll(digup, digforward, digdown)
+    log.info("dig3", i)
+
+    if shouldUseTorch and inventory.searchItem(torch) and i % 6 == 0 then
+      place.down(inventory.searchItem(torch))
+    end
+
+    move.forward()
+
+    i = i + 1
+  end
+  parallel.waitForAll(digup, digdown)
+end
+
+
+main(getopt({...}, {
+  t = "boolean", torch = "boolean",
+
+}))
